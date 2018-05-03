0301bfb2b7f5500a7c7a2aa4d038e38d3fc620c0
Menn <info@mennstudio.com>
2018-05-03 21:23:51 +0700

    1.3.5 (php)

--- seed-social.php
+++ seed-social.php
@@ -3,7 +3,7 @@
 Plugin Name: Seed Social
 Plugin URI: https://github.com/SeedThemes/seed-social
 Description: Minimal Social Sharing WordPress Plugin
-Version: 1.3.4
+Version: 1.3.5
 Author: SeedThemes
 Author URI: https://www.seedthemes.com
 License: GPL2
