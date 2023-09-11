90499e70dcef6d8d41841558188f0638a3620fce
Junior <68366806+JRChanakarn@users.noreply.github.com>
2023-09-11 14:55:26 +0700

    update icon x

--- seed-social.php
+++ seed-social.php
@@ -109,7 +109,7 @@ function seed_social($echo = true, $css_class = '')
 
     $facebook_icon = '<svg role="img" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><title>Facebook icon</title><path d="M23.9981 11.9991C23.9981 5.37216 18.626 0 11.9991 0C5.37216 0 0 5.37216 0 11.9991C0 17.9882 4.38789 22.9522 10.1242 23.8524V15.4676H7.07758V11.9991H10.1242V9.35553C10.1242 6.34826 11.9156 4.68714 14.6564 4.68714C15.9692 4.68714 17.3424 4.92149 17.3424 4.92149V7.87439H15.8294C14.3388 7.87439 13.8739 8.79933 13.8739 9.74824V11.9991H17.2018L16.6698 15.4676H13.8739V23.8524C19.6103 22.9522 23.9981 17.9882 23.9981 11.9991Z"/></svg>';
 
-    $twitter_icon = '<svg role="img" width="24" height="24" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Twitter icon</title><path d="M23.954 4.569c-.885.389-1.83.654-2.825.775 1.014-.611 1.794-1.574 2.163-2.723-.951.555-2.005.959-3.127 1.184-.896-.959-2.173-1.559-3.591-1.559-2.717 0-4.92 2.203-4.92 4.917 0 .39.045.765.127 1.124C7.691 8.094 4.066 6.13 1.64 3.161c-.427.722-.666 1.561-.666 2.475 0 1.71.87 3.213 2.188 4.096-.807-.026-1.566-.248-2.228-.616v.061c0 2.385 1.693 4.374 3.946 4.827-.413.111-.849.171-1.296.171-.314 0-.615-.03-.916-.086.631 1.953 2.445 3.377 4.604 3.417-1.68 1.319-3.809 2.105-6.102 2.105-.39 0-.779-.023-1.17-.067 2.189 1.394 4.768 2.209 7.557 2.209 9.054 0 13.999-7.496 13.999-13.986 0-.209 0-.42-.015-.63.961-.689 1.8-1.56 2.46-2.548l-.047-.02z"/></svg>';
+    $twitter_icon = '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.97222 1C2.782 1 1 2.782 1 4.97222V19.0278C1 21.218 2.782 23 4.97222 23H19.0278C21.218 23 23 21.218 23 19.0278V4.97222C23 2.782 21.218 1 19.0278 1H4.97222ZM5.78385 5.88889H9.71787L12.6863 10.1321L16.3614 5.88889H17.5836L13.2377 10.921L18.2687 18.1111H14.3347L11.0655 13.4371L7.02876 18.1111H5.78027L10.5116 12.6457L5.78385 5.88889ZM7.68283 6.92492L14.8037 17.075H16.3697L9.2488 6.92492H7.68283Z" fill="currentColor"/></svg>';
 
     $line_icon = '<svg role="img" width="24" height="24" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>LINE icon</title><path d="M19.365 9.863c.349 0 .63.285.63.631 0 .345-.281.63-.63.63H17.61v1.125h1.755c.349 0 .63.283.63.63 0 .344-.281.629-.63.629h-2.386c-.345 0-.627-.285-.627-.629V8.108c0-.345.282-.63.63-.63h2.386c.346 0 .627.285.627.63 0 .349-.281.63-.63.63H17.61v1.125h1.755zm-3.855 3.016c0 .27-.174.51-.432.596-.064.021-.133.031-.199.031-.211 0-.391-.09-.51-.25l-2.443-3.317v2.94c0 .344-.279.629-.631.629-.346 0-.626-.285-.626-.629V8.108c0-.27.173-.51.43-.595.06-.023.136-.033.194-.033.195 0 .375.104.495.254l2.462 3.33V8.108c0-.345.282-.63.63-.63.345 0 .63.285.63.63v4.771zm-5.741 0c0 .344-.282.629-.631.629-.345 0-.627-.285-.627-.629V8.108c0-.345.282-.63.63-.63.346 0 .628.285.628.63v4.771zm-2.466.629H4.917c-.345 0-.63-.285-.63-.629V8.108c0-.345.285-.63.63-.63.348 0 .63.285.63.63v4.141h1.756c.348 0 .629.283.629.63 0 .344-.282.629-.629.629M24 10.314C24 4.943 18.615.572 12 .572S0 4.943 0 10.314c0 4.811 4.27 8.842 10.035 9.608.391.082.923.258 1.058.59.12.301.079.766.038 1.08l-.164 1.02c-.045.301-.24 1.186 1.049.645 1.291-.539 6.916-4.078 9.436-6.975C23.176 14.393 24 12.458 24 10.314"/></svg>';
 
@@ -293,39 +293,42 @@ add_action('admin_menu', 'seed_social_setup_menu');
 
 function seed_social_init()
 {
-    ?><style>
-form label {
-    display: inline-block;
-    min-width: 60px;
-    margin-right: 10px;
-}
+    ?>
+<style>
+	form label {
+		display: inline-block;
+		min-width: 60px;
+		margin-right: 10px;
+	}
 
-.form-table th,
-.form-table td {
-    padding: 0;
-    line-height: 4em;
-}
+	.form-table th,
+	.form-table td {
+		padding: 0;
+		line-height: 4em;
+	}
 
-.form-table td p.description {
-    margin-top: -10px;
-}
-
+	.form-table td p.description {
+		margin-top: -10px;
+	}
 </style>
 <div class="wrap">
-    <div class="icon32" id="icon-options-general"></div>
-    <h2><?php esc_html_e('Seed Social', 'seed-social'); ?></h2>
-    <p>
-        <?php printf(wp_kses(__('For more information, please visit <a href="%1s" target="_blank">FAQ on WordPress.org</a>.', 'seed-social'), array( 'a' => array( 'href' => array(), 'target' => array() ) )), esc_url('https://wordpress.org/plugins/seed-social/#faq')); ?>
-    </p>
-    <form action="<?php echo admin_url('options.php'); ?>" method="post" id="seed-social-form">
-        <?php
+	<div class="icon32" id="icon-options-general"></div>
+	<h2><?php esc_html_e('Seed Social', 'seed-social'); ?>
+	</h2>
+	<p>
+		<?php printf(wp_kses(__('For more information, please visit <a href="%1s" target="_blank">FAQ on WordPress.org</a>.', 'seed-social'), array( 'a' => array( 'href' => array(), 'target' => array() ) )), esc_url('https://wordpress.org/plugins/seed-social/#faq')); ?>
+	</p>
+	<form
+		action="<?php echo admin_url('options.php'); ?>"
+		method="post" id="seed-social-form">
+		<?php
                 settings_fields('seed-social');
-                do_settings_sections('seed-social');
-                submit_button();
-        ?>
-    </form>
+    do_settings_sections('seed-social');
+    submit_button();
+    ?>
+	</form>
 </div>
-    <?php
+<?php
 }
 
 /**
@@ -506,8 +509,8 @@ function seed_social_get_post_types_option_list()
 
     foreach (get_post_types(array( '_builtin' => false, 'public' => true ), 'objects') as $_slug => $_post_type) {
         if (
-            ( ( ! is_woo_activated() ) || ( $_post_type->name != 'product' ) ) &&
-            ( $_post_type->name != 'seed_confirm_log' )
+            ((! is_woo_activated()) || ($_post_type->name != 'product')) &&
+            ($_post_type->name != 'seed_confirm_log')
         ) {
             $list[ $_slug ] = $_post_type->labels->name ;
         }
@@ -534,8 +537,12 @@ function seed_social_output_settings_field($option)
 
     switch ($field_type) :
         case 'text':
-            ?><input type="text" name="<?php echo $option['name']; ?>" id="<?php echo $id; ?>" value="<?php echo sanitize_text_field($current); ?>" class="regular-text" />
-            <?php
+            ?><input type="text"
+	name="<?php echo $option['name']; ?>"
+	id="<?php echo $id; ?>"
+	value="<?php echo sanitize_text_field($current); ?>"
+	class="regular-text" />
+<?php
             break;
 
         case 'checkbox':
@@ -545,43 +552,55 @@ function seed_social_output_settings_field($option)
                 }
 
                 $selected = is_array($current) && in_array($val, $current) ? 'checked="checked"' : '';
-                ?><label for="<?php echo $id; ?>"><input type="checkbox" name="<?php echo $option['name']; ?>[]" value="<?php echo $val; ?>" id="<?php echo $id; ?>" <?php echo $selected; ?> />
-                <?php echo $choice; ?></label><?php
+                ?><label
+	for="<?php echo $id; ?>"><input type="checkbox"
+		name="<?php echo $option['name']; ?>[]"
+		value="<?php echo $val; ?>"
+		id="<?php echo $id; ?>"
+		<?php echo $selected; ?> />
+	<?php echo $choice; ?></label><?php
             endforeach;
             break;
 
         case 'dropdown':
             ?>
 <label for="<?php echo $option['name']; ?>">
-    <select name="<?php echo $option['name']; ?>" id="<?php echo $id; ?>">
-            <?php
+	<select name="<?php echo $option['name']; ?>"
+		id="<?php echo $id; ?>">
+		<?php
             foreach ($option['options'] as $val => $choice) :
                 if ($val == $current) {
                     $selected = 'selected="selected"';
                 } else {
-                    $selected = ''; ?><option value="<?php echo $val; ?>" <?php echo $selected; ?>><?php echo $choice; ?></option><?php
+                    $selected = ''; ?>
+		<option value="<?php echo $val; ?>" <?php echo $selected; ?>><?php echo $choice; ?>
+		</option><?php
                 }
             endforeach;
             ?>
-    </select>
+	</select>
 </label>
-            <?php
+<?php
             break;
 
         case 'textarea':
             if (!$current && isset($option['std'])) {
                 $current = $option['std'];
             } ?>
-<textarea name="<?php echo $option['name']; ?>" id="<?php echo $id; ?>" rows="8" cols="70"><?php echo $current; ?></textarea>
-            <?php
+<textarea name="<?php echo $option['name']; ?>"
+	id="<?php echo $id; ?>" rows="8"
+	cols="70"><?php echo $current; ?></textarea>
+<?php
             break;
 
         case 'textarea_code':
             if (!$current && isset($option['std'])) {
                 $current = $option['std'];
             } ?>
-<textarea name="<?php echo $option['name']; ?>" id="<?php echo $id; ?>" rows="4" cols="60" class="code" readonly><?php echo $current; ?></textarea>
-            <?php
+<textarea name="<?php echo $option['name']; ?>"
+	id="<?php echo $id; ?>" rows="4" cols="60" class="code"
+	readonly><?php echo $current; ?></textarea>
+<?php
             break;
     endswitch;
 
@@ -615,9 +634,10 @@ function seed_social_custom_box_html($post)
 {
     $value = get_post_meta(get_the_ID(), '_seed_social_disable', true);
     ?>
-<input type="checkbox" name="seed_social_disable" id="seed_social_disable" class="postbox" <?php checked($value, 'on'); ?> />
+<input type="checkbox" name="seed_social_disable" id="seed_social_disable" class="postbox"
+	<?php checked($value, 'on'); ?> />
 <label for="seed_social_disable">Disable social sharing button</label>
-    <?php
+<?php
 }
 
 function seed_social_save_postdata($post_id)
@@ -638,3 +658,4 @@ function seed_social_save_postdata($post_id)
 add_action('save_post', 'seed_social_save_postdata');
 
 load_plugin_textdomain('seed-social', false, basename(dirname(__FILE__)) . '/languages');
+?>
